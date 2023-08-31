432a948e430a461f5f54aaddc3a7b3a79ba2f125
Schmelzberry <jelsberry96@gmail.com>
2023-08-31 16:16:40 -0700

    write test 1 - RED

---
+++ __tests__/haiku.test.js
@@ -0,0 +1,10 @@
+// import Haiku from './../src/haiku.js';
+
+describe('Haiku', () => {
+
+  test('should correctly create a string', () => {
+    const sentence = new Haiku()
+    expect(sentence).toEqual(undefined)
+
+  });
+});
